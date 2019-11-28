f842cde3a08cd2673efd64950ed1666a7dd9ecbd
youngcitrus <mrjustinnguyen@gmail.com>
2019-11-27 23:19:19 -0800

    fetching spotify new releases data, additional track information commented out, and displaying album art for new releases

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const client_secret = 'fc3d2d3ece504bea8f552847bf0b5c44'; // Your secret
   document.addEventListener('DOMContentLoaded', () => {
   console.log('hi');
 
+  //request auth token from Spotify API
+
   const authOptions = {
       url: 'https://cors-anywhere.herokuapp.com/https://accounts.spotify.com/api/token',
       headers: {
@@ -21,26 +23,78 @@ const client_secret = 'fc3d2d3ece504bea8f552847bf0b5c44'; // Your secret
     if (!error && response.statusCode === 200) {
   
       // use the access token to access the Spotify Web API
-      var token = body.access_token;
-      var options = {
-        url: 'https://cors-anywhere.herokuapp.com/https://api.spotify.com/v1/browse/new-releases?limit=50',
+      // request 50 latest album/single releases
+
+      let token = body.access_token;
+      let options = {
+        url: 'https://cors-anywhere.herokuapp.com/https://api.spotify.com/v1/browse/new-releases?limit=49',
         headers: {
           'Authorization': 'Bearer ' + token
         },
         json: true
       };
+
+      const tracks = [];
+      const albums = [];
+
       request.get(options, function(error, response, body) {
-        body.albums.items.forEach(album => {console.log(album)})
-        const svg = d3.select("#test").append("svg")
-          .attr("width", 400)
-          .attr("height", 400)
-        const rectangle = svg.append("rect")
-          .attr("x", 200)
-          .attr("y", 200)
-          .attr("width", 70)
-          .attr("height", 70)
-          .attr("fill", "black")
         
+        console.log(body);
+
+        const svg = d3.select("#main").append("svg")
+          .attr("width", 448)
+          .attr("height", 448)
+
+        body.albums.items.forEach((album,i) => {
+          let xPos = (i % 7) * 64;
+          let yPos = Math.floor(i / 7) * 64;
+
+          let albumArt = svg.append('image')
+          .attr('xlink:href', album.images[2].url)
+          .attr('width', 64)
+          .attr('height', 64)
+          .attr('x', xPos)
+          .attr('y', yPos)
+
+
+          // make API call to get tracks from albums
+          // let albumUrl = 'https://cors-anywhere.herokuapp.com/https://api.spotify.com/v1/albums/' + album.id + '/tracks';
+          // let albumOptions = {
+          //   url: albumUrl,
+          //   headers: {
+          //     'Authorization': 'Bearer ' + token
+          //   },
+          //   json: true
+          // };
+
+          
+          // request.get(albumOptions, function(error, response, body){
+          //   body.items.forEach(track => {
+          //     track.artists.forEach(artist => {console.log('artist:' + artist.name)});
+          //     console.log('track:' + track.name)
+
+              // make API call to get audio features of tracks
+              // let trackUrl = 'https://cors-anywhere.herokuapp.com/https://api.spotify.com/v1/audio-features/' + track.id;
+              
+              // let trackOptions = {
+              //   url: trackUrl,
+              //   headers: {
+              //     'Authorization': 'Bearer ' + token
+              //   },
+              //   json: true
+              // };
+              
+              // request.get(trackOptions, function(error, response, body) {
+              //   console.log(body);
+              // })
+
+          //   });
+          // });
+        });
+        const infoWindow = d3.select("#main").append("svg")
+          .attr("width", 448)
+          .attr("height", 448)
+          .attr('x', 448)
       });
     }
   });
